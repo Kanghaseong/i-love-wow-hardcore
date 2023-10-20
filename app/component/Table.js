d867271ef5214c620ccef04dbecdd0d4f154fd49
khs <hskang200@gmail.com>
2023-10-21 00:00:24 +0900

    mobile css

--- app/component/Table.js
+++ app/component/Table.js
@@ -1,60 +1,59 @@
 "use client";
+// React Component
 import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./table.module.css";
 import env_config from "@/env_config";
 
 export default function TableComponent() {
-  const [data, setData] = useState([]); // 초기 상태는 빈 객체
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    // API 호출을 통해 데이터 가져오기
     axios
       .get(`${env_config()}/users`)
       .then((response) => {
         const sortedData = response.data.users.sort((a, b) => b.level - a.level);
-        // 가져온 데이터로 상태 업데이트 전에 정렬
-        console.log(sortedData);
         setData(sortedData);
       })
       .catch((error) => {
         console.error("There was an error fetching the data: ", error);
       });
   }, []);
-  //const arr = Object.values(data); // 객체의 값들을 배열로 변환
 
   return (
     <div className={styles.tableBox}>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>레벨</th>
-            <th>이름</th>
-            <th>클래스</th>
-            <th>종족</th>
-            <th>성별</th>
-            <th>진영</th>
-            <th>렐름</th>
-            <th>길드</th>
-            <th>마지막 접속</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index} className={row.is_fully_dead ? styles.dead : styles.undead}>
-              <td>{row.level}</td>
-              <td>{row.name}</td>
-              <td>{row.character_class}</td>
-              <td>{row.race}</td>
-              <td>{row.gender}</td>
-              <td>{row.faction}</td>
-              <td>{row.realm}</td>
-              <td>{row.guild}</td>
-              <td>{new Date(row.last_login_timestamp).toLocaleString()}</td>
+      <div className={styles.tableContainer}>
+        <table border="1" className={styles.table}>
+          <thead>
+            <tr>
+              <th>레벨</th>
+              <th>이름</th>
+              <th>클래스</th>
+              <th>종족</th>
+              <th>성별</th>
+              <th>진영</th>
+              <th>렐름</th>
+              <th>길드</th>
+              <th>마지막 접속</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index} className={row.is_fully_dead ? styles.dead : styles.undead}>
+                <td data-label="레벨">{row.level}</td>
+                <td data-label="이름">{row.name}</td>
+                <td data-label="클래스">{row.character_class}</td>
+                <td data-label="종족">{row.race}</td>
+                <td data-label="성별">{row.gender}</td>
+                <td data-label="진영">{row.faction}</td>
+                <td data-label="렐름">{row.realm}</td>
+                <td data-label="길드">{row.guild}</td>
+                <td data-label="마지막 접속">{new Date(row.last_login_timestamp).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
