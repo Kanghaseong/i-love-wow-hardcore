e50c9653b448a07cb91788b89f3924bc02bf85e4
khs <hskang200@gmail.com>
2023-10-20 23:26:30 +0900

    change table h3 note

--- app/dash/page.js
+++ app/dash/page.js
@@ -8,7 +8,7 @@ export default function Page() {
     <div className={styles.dash}>
       <img src="/logo.png" alt="logo" />
       <h3>*사망한 유저는 빨간색으로 표시됩니다.</h3>
-      <h3>*블리자드 API를 사용하기 때문에 정확한 갱신 기준을 현재 파악하지 못하고 있습니다.</h3>
+      <h3>*블리자드 API를 사용하기 때문에 정확한 마지막 접속 기준을 파악하지 못하고 있습니다.</h3>
       <Table></Table>
 
       <div className={styles.foot}>
