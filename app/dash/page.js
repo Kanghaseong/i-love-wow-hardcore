c3e1c0a01d54edc6dc484cc45651032777d68346
khs <hskang200@gmail.com>
2023-10-22 11:33:38 +0900

    add red color span, note ghost logic

--- app/dash/page.js
+++ app/dash/page.js
@@ -7,8 +7,12 @@ export default function Page() {
   return (
     <div className={styles.dash}>
       <img src="/logo.png" alt="logo" />
-      <h3>*사망한 유저는 빨간색으로 표시됩니다.</h3>
       <h3>*캐릭터 정보 갱신은 배틀넷 로그아웃시 적용됩니다.</h3>
+      <h3>
+        *사망한 유저는 <span className={styles.red_text}>빨간색</span>으로 표시됩니다.
+      </h3>
+      <h5>*캐릭터가 사망해도 "유령상태"로 존재해야 사망여부가 적용됩니다.</h5>
+
       <Table></Table>
 
       <div className={styles.foot}>
