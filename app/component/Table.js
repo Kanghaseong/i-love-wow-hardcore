7aeddf01be22517fe5585775e19617575deec88d
khs <hskang200@gmail.com>
2023-10-25 11:02:56 +0900

    del tabel container

--- app/component/Table.js
+++ app/component/Table.js
@@ -22,45 +22,43 @@ export default function TableComponent() {
 
   return (
     <div className={styles.tableBox}>
-      <div className={styles.tableContainer}>
-        <table border="1" className={styles.table}>
-          <thead>
-            <tr>
-              <th>레벨</th>
-              <th>이름</th>
-              <th>클래스</th>
-              <th>종족</th>
-              <th>성별</th>
-              <th>진영</th>
-              <th>렐름</th>
-              <th>길드</th>
-              <th>마지막 접속</th>
+      <table border="1" className={styles.table}>
+        <thead>
+          <tr>
+            <th>레벨</th>
+            <th>이름</th>
+            <th>클래스</th>
+            <th>종족</th>
+            <th>성별</th>
+            <th>진영</th>
+            <th>렐름</th>
+            <th>길드</th>
+            <th>마지막 접속</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index} className={row.is_ghost ? styles.dead : styles.undead}>
+              <td data-label="레벨">{row.level}</td>
+              <td data-label="이름">{row.name}</td>
+              <td data-label="클래스">{row.user_class}</td>
+              <td data-label="종족">{row.race}</td>
+              <td data-label="성별">{row.gender}</td>
+              <td data-label="진영">{row.faction}</td>
+              <td data-label="렐름">{row.realm}</td>
+              <td data-label="길드">{row.guild}</td>
+              <td data-label="마지막 접속">
+                {new Date(row.last_login_timestamp).toLocaleString("ko-KR", {
+                  month: "long",
+                  day: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index} className={row.is_ghost ? styles.dead : styles.undead}>
-                <td data-label="레벨">{row.level}</td>
-                <td data-label="이름">{row.name}</td>
-                <td data-label="클래스">{row.user_class}</td>
-                <td data-label="종족">{row.race}</td>
-                <td data-label="성별">{row.gender}</td>
-                <td data-label="진영">{row.faction}</td>
-                <td data-label="렐름">{row.realm}</td>
-                <td data-label="길드">{row.guild}</td>
-                <td data-label="마지막 접속">
-                  {new Date(row.last_login_timestamp).toLocaleString("ko-KR", {
-                    month: "long",
-                    day: "numeric",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
