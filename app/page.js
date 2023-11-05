222b192cb6b57ee785b1cd937d267e6089a89814
khs <hskang200@gmail.com>
2023-11-06 01:01:17 +0900

    change "/" route style

--- app/page.js
+++ app/page.js
@@ -5,9 +5,7 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.titles}>
-        <h1>Why is World of Warcraft Classic Hardcore fun?</h1>
-        <h3>월드 오브 워크래프트 클래식 하드코어는 왜 재밌을까?</h3>
-        <h5>ワールド·オブ·ワーク·ラフトクラシックハードコアはなぜ面白いのですか？</h5>
+        <h1>WXW</h1>
       </div>
 
       <Link href="dash" className={styles.start}>
