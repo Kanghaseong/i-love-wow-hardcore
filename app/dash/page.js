0936a6bf33de56060873361f5e9207a608c83ec0
khs <hskang200@gmail.com>
2023-11-05 16:07:24 +0900

    black
    change : all color, layout, fonts
    
    layout : sections...

--- app/dash/page.js
+++ app/dash/page.js
@@ -6,16 +6,27 @@ import Nav from "../component/Nav";
 export default function Page() {
   return (
     <div className={styles.dash}>
+      <section className={styles.section1}>
+        <div className={styles.none}>none</div>
+        <nav className={styles.nav_head}>
+          <h1>
+            <span>WXW</span>
+          </h1>
+          <li>
+            <span class="material-symbols-outlined">sort</span>Dashboard
+          </li>
+          <li>
+            <span class="material-symbols-outlined">refresh</span>Refresh
+          </li>
+        </nav>
+      </section>
+      <section className={styles.section2}>섹션 2</section>
+      <section className={styles.section3}>
+        <div>디브</div>
+        <div className={styles.none}>none</div>
+      </section>
       {/* <Nav /> */}
-      <Logo />
-      <div className={styles.titles}>
-        <h3>*캐릭터 정보 갱신은 배틀넷 로그아웃시 적용됩니다.</h3>
-        <h3>
-          *사망한 유저는 <span className={styles.red_text}>빨간색</span>으로 표시됩니다.
-        </h3>
-        <h5>*캐릭터가 사망해도 "유령상태"로 존재해야 사망여부가 적용됩니다.</h5>
-      </div>
-      <Table></Table>
+      {/* <Table></Table> */}
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Orbit } from "next/font/google";
+import { Victor_Mono } from "next/font/google";
 
-const inter = Orbit({ subsets: ["latin"], weight: "400" });
+const inter = Victor_Mono({ subsets: ["vietnamese"], weight: "400" });
 
 export const metadata = {
   title: "I love world of warcraft classic hardcore.",
@@ -11,6 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
